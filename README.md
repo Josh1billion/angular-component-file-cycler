64c329e993fd227498cd1fc7bfcf17ca7bf4724a
Josh1billion <josh1billion@yahoo.com>
2018-01-19 16:42:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Angular Component Cycler
+
+This allows you to switch between similarly-named Angular component files with an easy shortcut.
+
+For example, if you have hello.component.ts open, you can activate this extension (press Ctrl+K, then Ctrl+O without ever releasing Ctrl) to tell VS Code to open hello.component.html.  Activating it again will open the corresponding scss or css file.  Activating it once more will open the corresponding ts file again.
+
+##Feedback / bug reports
+
+Contact me at @Fordesoft on twitter, or you can contact me through http://www.fordesoft.com/
+
---
+++ src/extension.ts
@@ -0,0 +1,56 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    let disposable = vscode.commands.registerCommand('extension.cycleAngularComponentFiles', () => {
+        
+        var currentFilename: string = vscode.window.activeTextEditor.document.fileName;
+        var currentFileExtension: string = currentFilename.substr(currentFilename.lastIndexOf('.') + 1).toLowerCase();
+        var currentFilenameWithoutExtension: string = currentFilename.substr(0, currentFilename.lastIndexOf('.') + 1);
+
+        // if the current file doesn't appear to be an Angular component file, return
+        if (currentFileExtension != "ts" && currentFileExtension != "html" && currentFileExtension != "scss" && currentFileExtension != "css") {
+            return;
+        }
+
+        // figure out what the next file to switch to should be
+        var newFileExtension: string = "";
+        if (currentFileExtension == "ts" && fileExists(currentFilenameWithoutExtension + "html")) {
+            newFileExtension = "html";
+        }
+        else if (currentFileExtension == "html" && fileExists(currentFilenameWithoutExtension + "scss")) {
+            newFileExtension = "scss";
+        }
+        else if (currentFileExtension == "html" && fileExists(currentFilenameWithoutExtension + "css")) {
+            newFileExtension = "css";
+        }
+        else if (currentFileExtension == "scss" && fileExists(currentFilenameWithoutExtension + "ts")) {
+            newFileExtension = "ts";
+        }
+        else if (currentFileExtension == "css" && fileExists(currentFilenameWithoutExtension + "ts")) {
+            newFileExtension = "ts";
+        }
+
+        // if an appropriate next file was found, switch to it
+        if (newFileExtension.length > 0) {
+            vscode.workspace.openTextDocument(currentFilenameWithoutExtension + newFileExtension)
+            .then(doc => vscode.window.showTextDocument(doc));
+        }
+        
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
+
+function fileExists(filename: string): boolean {
+    return fs.existsSync(filename);
+}
